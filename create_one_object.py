7192ab7e32ac7f800aa0c06f6e52424636c718ca
habibulloabdullayev <habibulloabdullayev02@gmail.com>
2023-09-09 16:42:57 +0500

    create_one_object

--- create_one_object.py
+++ create_one_object.py
@@ -1,3 +1,6 @@
-from create_one_attribute import Person
-
 #create an object named "person" whose name is "Ali"
+class Person:
+    def __init__(self,name):
+        self.name = name
+name_1 = Person('Ali')
+print(name_1.name)
