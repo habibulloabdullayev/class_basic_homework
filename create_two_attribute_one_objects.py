6b527830afddcd38d06bebaf7410624a6f8d0731
habibulloabdullayev <habibulloabdullayev02@gmail.com>
2023-09-09 16:45:59 +0500

    create_two_attribute_one_objects

--- create_two_attribute_one_objects.py
+++ create_two_attribute_one_objects.py
@@ -1,3 +1,6 @@
 from create_two_attribute import Person
 
 #create an object named "person" whose name is "Ali", age is "25"
+name = Person('Ali',25).name
+age = Person('Ali',25).age
+print(name,age) 
